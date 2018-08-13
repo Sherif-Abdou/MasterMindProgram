b440d42372f95f6d2e51779d9f341e9ac0137842
Sherif <portal.meteor.master@gmail.com>
2018-08-13 14:07:15 -0700

    Can give an answer to a guess

--- MasterMind/MasterMindGame.cpp
+++ MasterMind/MasterMindGame.cpp
@@ -13,4 +13,32 @@ MasterMindGame::~MasterMindGame()
 
 Answer MasterMindGame::getAnswer(Guess guess)
 {
+	Answer answer = Answer();
+	int i = 0;
+	for (auto color: guess)
+	{
+		if (color == this->code[i])
+		{
+			answer.push_back(Peg::Black);
+			continue;
+		}
+		else {
+			bool colorFound = false;
+			for (auto codeColor: this->code)
+			{
+				if (color == codeColor)
+				{
+					answer.push_back(Peg::White);
+					colorFound = true;
+					break;
+				}
+			}
+			if (!colorFound)
+			{
+				answer.push_back(Peg::None);
+			}
+		}
+	}
+	this->tries++;
+	return answer;
 }
